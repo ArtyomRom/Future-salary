d61e02ea12f3be55d2abbdbe42e89e1ff0ce8fb1
Artyom <zik21b@gmail.com>
2025-01-19 13:11:00 +0400

    Убрал повторяющие функции

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-import requests
 from datetime import datetime, timedelta
+
+import requests
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
@@ -31,7 +32,8 @@ def get_vacancies_by_language(language: str):
     moscow_id = 1
     quantity_per_page = 100
     date_from = (today - timedelta(days=30)).strftime("%Y-%m-%dT%H:%M:%S")
-    params = {'text': f"{text} {language}", 'per_page': quantity_per_page, 'page': 1, 'area': moscow_id, 'date_from': date_from}
+    params = {'text': f"{text} {language}", 'per_page': quantity_per_page, 'page': 1, 'area': moscow_id,
+              'date_from': date_from}
     url = 'https://api.hh.ru/vacancies/'
     vacancies = []
     while True:
@@ -43,7 +45,6 @@ def get_vacancies_by_language(language: str):
                 vacancy['found'] = response['found']
                 vacancies.append(vacancy)
 
-
             if params['page'] >= response['pages']:
                 break
             else:
@@ -54,16 +55,15 @@ def get_vacancies_by_language(language: str):
     return vacancies
 
 
-def predict_rub_salary(vacancy):
-    rub_salary = vacancy.get('salary', False)
-    if not rub_salary or rub_salary['currency'] != 'RUR':
+def predict_rub_salary(vacancy, salary_from, salary_to):
+    if vacancy['currency'] not in ('RUR', 'rub'):
         return None
-    if rub_salary['from'] and rub_salary['to']:
-        return (rub_salary['from'] + rub_salary['to']) / 2
-    elif rub_salary['from']:
-        return rub_salary['from'] * 1.2
-    elif rub_salary['to']:
-        return rub_salary['to'] * 0.8
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    elif salary_from:
+        return salary_from * 1.2
+    elif salary_to:
+        return salary_to * 0.8
 
 
 def get_statistics_on_programming_languages():
@@ -74,7 +74,10 @@ def get_statistics_on_programming_languages():
         vacancies_found = set(map(lambda x: x["found"], vacancies))
         if not vacancies_found or min(vacancies_found) < minimum_number_of_vacancies:
             continue
-        salaries_by_vacancy = [predict_rub_salary(vacancy) for vacancy in vacancies]
+        salaries_by_vacancy = [predict_rub_salary(vacancy['salary'], vacancy['salary']['from'], vacancy['salary']['to'])
+                               for
+                               vacancy in vacancies if vacancy.get('salary', False)]
+
         salaries_by_vacancy = [salary for salary in salaries_by_vacancy if salary]
         average_salary = sum(salaries_by_vacancy) / len(salaries_by_vacancy) if salaries_by_vacancy else 0
         staticstics_languages[language] = {
@@ -82,11 +85,14 @@ def get_statistics_on_programming_languages():
             "vacancies_processed": len(salaries_by_vacancy),
             "average_salary": int(average_salary),
         }
-    title = '-HeadHunter Moscow-'
+    return staticstics_languages
+
+
+def enter_statistics(title, statistics):
     table_data = (
         ('Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'),
         *((language, value['vacancies_found'], value['vacancies_processed'], value['average_salary'])
-          for language, value in staticstics_languages.items())
+          for language, value in statistics.items())
     )
 
     table = AsciiTable(table_data, title=title)
@@ -95,5 +101,6 @@ def get_statistics_on_programming_languages():
 
 if __name__ == '__main__':
     load_dotenv()
-    get_statistics_on_programming_languages()
-    print(get_statistics_on_programming_languages_sj(os.getenv('SECRET_KEY')))
+    enter_statistics('-HeadHunter Moscow-', get_statistics_on_programming_languages())
+    enter_statistics('-SuperJob Moscow-',
+                     get_statistics_on_programming_languages_sj(os.getenv('SECRET_KEY')))
--- superjob.py
+++ superjob.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
-from terminaltables import AsciiTable
+
 import requests
+
 
 def group_vacancies_by_language_sj(secret_key):
     vacancies_by_language = {
@@ -20,17 +21,6 @@ def group_vacancies_by_language_sj(secret_key):
     return vacancies_by_language
 
 
-def predict_rub_salary_sj(vacancy):
-    if vacancy['currency'] != 'rub':
-        return None
-    if vacancy['payment_from'] and vacancy['payment_to']:
-        return (vacancy['payment_from'] + vacancy['payment_to']) / 2
-    elif vacancy['payment_from']:
-        return vacancy['payment_from'] * 1.2
-    elif vacancy['payment_to']:
-        return vacancy['payment_to'] * 0.8
-
-
 def get_vacancies_sj(languange: str, secret_key):
     vacancies_sj = []
     headers = {'X-Api-App-Id': secret_key}
@@ -40,7 +30,8 @@ def get_vacancies_sj(languange: str, secret_key):
     page = 0
     while True:
         try:
-            params = {'keywords': languange, 'catalogues': categoria_id, 'town': 'Москва', 'page': page, 'count': quantity_per_page,
+            params = {'keywords': languange, 'catalogues': categoria_id, 'town': 'Москва', 'page': page,
+                      'count': quantity_per_page,
                       'date_published[from]': date_from}
             response = requests.get(f'https://api.superjob.ru/2.0/vacancies/', headers=headers, params=params)
             response.raise_for_status()
@@ -60,13 +51,16 @@ def get_vacancies_sj(languange: str, secret_key):
 
 
 def get_statistics_on_programming_languages_sj(secret_key: str):
+    from main import predict_rub_salary
     vacancies_by_language = group_vacancies_by_language_sj(secret_key)
     staticstics_languages = {}
     for language, vacancies in vacancies_by_language.items():
         vacancies_found = set(map(lambda vacancy: vacancy['total'], vacancies))
         if not vacancies_found:
             continue
-        salaries_by_vacancy = [predict_rub_salary_sj(vacancy) for vacancy in vacancies]
+        salaries_by_vacancy = [
+            predict_rub_salary(vacancy, vacancy['payment_from'], vacancy['payment_to']) for vacancy
+            in vacancies]
         salaries_by_vacancy = [salary for salary in salaries_by_vacancy if salary]
         average_salary = sum(salaries_by_vacancy) / len(salaries_by_vacancy) if salaries_by_vacancy else 0
         staticstics_languages[language] = {
@@ -74,14 +68,4 @@ def get_statistics_on_programming_languages_sj(secret_key: str):
             "vacancies_processed": len(salaries_by_vacancy),
             "average_salary": average_salary,
         }
-    title = '-SuperJob Moscow-'
-    table_data = (
-        ('Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'),
-        *((language, value['vacancies_found'], value['vacancies_processed'], value['average_salary'])
-          for language, value in staticstics_languages.items())
-    )
-
-    table = AsciiTable(table_data, title=title)
-    return (table.table)
-
-
+    return staticstics_languages
